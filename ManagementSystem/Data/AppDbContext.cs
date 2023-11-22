48f0a83df5cdb33342f69a96dac148864d4634ee
Hanan Maqbol <hananmaqbol1@outlook.com>
2023-11-22 19:47:40 +0500

    modified code in the AppDbContext file as nugget console was unable to get DbContext file becuase of missing keyword "base".

--- ManagementSystem/Data/AppDbContext.cs
+++ ManagementSystem/Data/AppDbContext.cs
@@ -4,9 +4,11 @@ namespace ManagementSystem.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext>options)
+        public AppDbContext(DbContextOptions<AppDbContext>options) : base(options) 
         {
-            
+
         }
+
+        
     }
 }
