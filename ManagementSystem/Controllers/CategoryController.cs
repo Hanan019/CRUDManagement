0de70897e12fb0c95c2eb294797a8d75c30f9d56
Hanan Maqbol <hananmaqbol1@outlook.com>
2023-11-26 17:15:26 +0500

    added some bootstrap classes to _Layout for styling of Navbar and footer as well. and edited small changes in views.

--- ManagementSystem/Controllers/CategoryController.cs
+++ ManagementSystem/Controllers/CategoryController.cs
@@ -15,7 +15,7 @@ namespace ManagementSystem.Controllers
         public IActionResult Index()
         {
             List<Category> objCategoryList = _db.Categories.ToList();
-            return View();
+            return View(objCategoryList);
         }
     }
 }
